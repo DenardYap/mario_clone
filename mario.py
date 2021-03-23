6021c5b16fd66733604208e8e1f65ded2e0a9968
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2021-03-23 21:54:54 +0800

    Update mario.py

--- mario.py
+++ mario.py
@@ -25,6 +25,20 @@ clock = pygame.time.Clock()
 # import images & animation here
 # Tips: The bricks (floor) is 48px height
 
+<<<<<<< HEAD
+=======
+
+
+# goomba
+goomba_alive = True
+goomba_animation_i = 0
+goomba_animation_list = []
+goomba_animation_list.append(pygame.image.load("./animate_images/goomba0.png"))
+goomba_animation_list.append(pygame.image.load("./animate_images/goomba1.png"))
+goomba_hitbox = goomba_animation_list[0].get_rect(topleft = (700, 448-48-32))
+goomba_death_ani = pygame.image.load("./static_images/goomba_died.png")
+
+>>>>>>> parent of 60151a4 (Revert "Pull suggestions")
 # import sounds here
 
 
@@ -35,5 +49,37 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+<<<<<<< HEAD
+=======
+        # get mario events
+
+
+
+
+    # goomba
+    # goomba move
+    if goomba_alive == True:
+        goomba_hitbox.x -= 2
+
+    # goomba animation
+    goomba_animation_i += 0.1
+    if goomba_animation_i >= 2:
+        goomba_animation_i = 0
+
+    # goomba render
+    if goomba_alive == True:
+        screen.blit(goomba_animation_list[int(goomba_animation_i)], goomba_hitbox)
+    else:
+        screen.blit(goomba_death_ani, (goomba_hitbox.x, (goomba_hitbox.y + int(goomba_hitbox.height / 2))))
+
+    # check collsion
+    if (mario_hitbox.collidepoint((goomba_hitbox.x + 4, goomba_hitbox.y - 1)) or mario_hitbox.collidepoint((goomba_hitbox.x + goomba_hitbox.width - 4, goomba_hitbox.y - 1)) or mario_hitbox.collidepoint(goomba_hitbox.x + (int(goomba_hitbox.width / 2)), goomba_hitbox.y - 1)) == True:
+        goomba_alive = False
+    elif (mario_hitbox.colliderect(goomba_hitbox) and goomba_alive) == True:
+        print("Game over!", goomba_animation_i)
+        sys.exit()
+
+
+>>>>>>> parent of 60151a4 (Revert "Pull suggestions")
     pygame.display.update()
     clock.tick(60) #limit our game to 60 fps no matter what
