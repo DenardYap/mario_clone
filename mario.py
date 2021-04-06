fbf719987f36aeb9abd97f47eeac833b5a790b54
nazrinhz <81621811+nazrinhz@users.noreply.github.com>
2021-04-06 16:48:34 +0800

    Code for background movement
    Removed the limit logic as it is irrelevant. Fixed the import command.

--- mario.py
+++ mario.py
@@ -13,9 +13,15 @@ https://supermarioplay.com/
 import pygame
 import sys
 
+#initializing pygame
 pygame.init()
+
 # create a screen instance/object with 
 # 1000px width and 448px height
+screenW = 1000
+screenH = 448
+screen = pygame.display.set_mode((screenW,screenH))
+
 
 # I forgot to mention 'Clock'
 # the 'clock' instance help us to control the FPS
@@ -23,17 +29,65 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # import images & animation here
+bg = pygame.image.load("static_images/background.png")
+mario = pygame.image.load("static_images/mario.png")
+
 # Tips: The bricks (floor) is 48px height
 
 # import sounds here
 
-
-
 # import fonts here
 
+#variables
+marioW = 24    #mario width
+marioH = 32    #mario height
+bg_x_pos = 0   #background position
+bgW = 6784     #bacgkround width
+bgH = 448      #background height
+bgLimit = screenW - bgW
+mario_x_pos = 0
+mario_y_pos = 368
+marioWalkR =  False
+marioWalkL = False
+marioSpeed = 10
+
+
+
+#event
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    pygame.display.update()
-    clock.tick(60) #limit our game to 60 fps no matter what
+
+        if event.type == pygame.KEYDOWN:     #user pressed a key      
+            if event.key == pygame.K_d:      #user pressed the D key
+                marioWalkR = True
+            if event.key == pygame.K_a:      #user pressed the A key
+                marioWalkL = True
+        
+        if event.type == pygame.KEYUP:       #user released a key
+            if event.key == pygame.K_d:      #user released the D key
+                marioWalkR = False
+            if event.key == pygame.K_a:      #user released the A key
+                marioWalkL = False
+
+    #Mario walk right logic       
+    if marioWalkR == True:
+        if bg_x_pos > bgLimit:                   #logic to stop when limit is reached
+            if mario_x_pos < (575 - marioW):     #limit mario's movement to before the mid section
+                mario_x_pos += marioSpeed   
+            else:
+                bg_x_pos -= marioSpeed           #after reaching the middle, background will move
+
+
+
+
+    if marioWalkL == True:
+        if mario_x_pos > 0:                        #make sure he does not go off screen
+            mario_x_pos -= marioSpeed
+
+    screen.blit(bg, (bg_x_pos,0))                  #paint background
+    screen.blit(mario, (mario_x_pos,mario_y_pos))  #paint mario
+    pygame.display.update()                        #update screen
+    clock.tick(60)                                 #limit our game to 60 fps no matter what
+
