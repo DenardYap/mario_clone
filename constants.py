81c3949554ca07e3c3d2a81ea7cb4f9cfb09b99e
bernard <49737780+DenardYap@users.noreply.github.com>
2021-04-22 14:27:02 +0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,75 @@
+
+### NAZRIN's ### 
+screenW = 1000
+screenH = 448
+marioW = 24    #mario width
+marioH = 32    #mario height
+bg_x_pos = 0   #background position
+bgW = 6784     #bacgkround width
+bgH = 448      #background height
+mario_x_pos = 0
+mario_y_pos = 368
+marioWalkR =  False
+marioWalkL = False
+# marioSpeed = 10
+
+
+### EEJOY's ###
+
+mario_speed = 5.0
+count = 0
+
+draw_coin = False
+draw_star = False
+count_coin = 0
+count_star = 0
+count_star_rect = 0
+
+draw_coin1 = False
+draw_coin2 = False
+draw_coin3 = False
+draw_coin4 = False
+draw_coin5 = False
+draw_coin6 = False
+draw_coin7 = False
+draw_coin8 = False
+draw_coin9 = False
+
+count_coin1 = 0
+count_coin2 = 0
+count_coin3 = 0
+count_coin4 = 0
+count_coin5 = 0
+count_coin6 = 0
+count_coin7 = 0
+count_coin8 = 0
+count_coin9 = 0
+
+draw_mushroom1 = False
+draw_mushroom2 = False
+draw_mushroom3 = False
+
+count_mushroom1 = 0
+count_mushroom2 = 0
+count_mushroom3 = 0
+
+draw_empty_brick1 = False
+draw_empty_brick2 = False
+draw_empty_brick3 = False
+draw_empty_brick4 = False
+draw_empty_brick5 = False
+draw_empty_brick6 = False
+draw_empty_brick7 = False
+draw_empty_brick8 = False
+draw_empty_brick9 = False
+draw_empty_brick10 = False
+draw_empty_brick11 = False
+draw_empty_brick12 = False
+###############
+
+### BEN's ###
+
+goomba_alive = True
+goomba_animation_i = 0
+
+#############
