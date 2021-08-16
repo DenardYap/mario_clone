b12e5b1bb8aeacd87428c8eb2ce3f7ea98abedb5
Darshxx13 <81164868+Darshxx13@users.noreply.github.com>
2021-08-17 02:04:26 +0800

    Update resources.py

--- resources.py
+++ resources.py
@@ -270,7 +270,7 @@ mushroom_hit_box_list_y = [question_rect2, question_rect7, question_rect10, ques
                             brick_rect2, brick_rect3, brick_rect5, floor1, floor2, floor3, floor4]
 
 goomba_hitbox_x = [pipe1_rect, pipe2_rect, pipe3_rect, pipe4_rect, pipe5_rect, pipe6_rect]
-goomba_hitbox_y = [floor1, floor2, floor3, floor4, brick_rect4, brick_rect5, brick_rect6, brick_rect7, brick_rect8, brick_rect9, brick_rect10, brick_rect11, brick_rect12]      
+goomba_hitbox_y = [floor1, floor2, floor3, floor4, brick_rect4, brick_rect5, brick_rect6, brick_rect7, brick_rect8, brick_rect9, brick_rect10, brick_rect11, brick_rect12, question_rect11]      
               
 # Import star
 star = []
